1a9ee39fe441c6eed35b89a0d1f8d3cb70ad7e33
Neha Geete <nehageete92@gmail.com>
2026-07-11 14:24:12 +0530

    Time: 379 ms (100%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,7 +1,8 @@
--- Write your PostgreSQL query statement below
-select user_id , CONCAT(
-    upper(SUBSTRING(name , 1,1)),
-    lower(SUBSTRING(name,2))
+select user_id , CONCAT( -- dono 1st and last part ko jodega 
+    upper(SUBSTRING(name , 1,1)),   -- only first letter ko capital krega
+    lower(SUBSTRING(name,2)) -- 2nd letter se end tak ko small krega 
 ) as name 
 from Users
 order by user_id;
+
+-- SELECT INITCAP(name) FROM employees;-> ye build in function in postgre jo start letter ko capital aur baaki small banata hai eg. rAhUL shARMa -> Rahul Sharma
